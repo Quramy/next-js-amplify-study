e1e53928eb25cacae0bd677568c652c913339ffa
Quramy <yosuke.kurami@gmail.com>
2024-04-19 22:21:22 +0900

    Add Pages Router ISR page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ export const revalidate = 30;
 
 export default function Home() {
   const now = Date.now();
-  console.log("Render Home", new Date(now).toLocaleString());
+  console.log("Render index", new Date(now).toLocaleString());
   const renderedAt = new Date(now).toISOString();
   return (
     <div>
-      <p>Hi</p>
+      <p>App Router Page</p>
       <p>Rendered at: {renderedAt}</p>
     </div>
   );
---
+++ src/pages/test-page/index.tsx
@@ -0,0 +1,21 @@
+import { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log("execute getStaticProps");
+  return {
+    props: {
+      now: Date.now(),
+    },
+    revalidate: 30,
+  };
+};
+
+export default function Home({ now }: { now: number }) {
+  const renderedAt = new Date(now).toISOString();
+  return (
+    <div>
+      <p>Pages Router Page</p>
+      <p>Rendered at: {renderedAt}</p>
+    </div>
+  );
+}
